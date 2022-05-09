595508d504a08357e43aaa83b5d4b26b14b36bce
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2022-05-09 16:33:30 -0700

    more flexibility if you want to just get the series instance uids, or the file names too

--- DicomParser.cs
+++ DicomParser.cs
@@ -6,22 +6,31 @@ namespace DicomFolderParser
 {
     public class DicomParser
     {
-        public Dictionary<string, VectorString> series_instance_uids = new Dictionary<string, VectorString>();
+        public Dictionary<string, VectorString> series_instance_uids_dict = new Dictionary<string, VectorString>();
+        public VectorString dicom_series_instance_uids;
         public DicomParser()
         {
         }
         public void __reset__()
         {
-            series_instance_uids = new Dictionary<string, VectorString>();
+            series_instance_uids_dict = new Dictionary<string, VectorString>();
+        }
+        public void GetSeriesInstanceUIDs(string directory)
+        {
+            dicom_series_instance_uids = ImageSeriesReader.GetGDCMSeriesIDs(directory);
+        }
+        public void GetFileNames(string directory)
+        {
+            foreach (string dicom_series_id in dicom_series_instance_uids)
+            {
+                VectorString dicom_names = ImageSeriesReader.GetGDCMSeriesFileNames(directory, dicom_series_id);
+                series_instance_uids_dict.Add(dicom_series_id, dicom_names);
+            }
         }
         public void ParseDirectory(string directory)
         {
-            VectorString dicom_series_ids = ImageSeriesReader.GetGDCMSeriesIDs(directory);
-            foreach (string dicom_series_id in dicom_series_ids)
-            {
-                VectorString dicom_names = ImageSeriesReader.GetGDCMSeriesFileNames(directory, dicom_series_id);
-                series_instance_uids.Add(dicom_series_id, dicom_names);
-            }
+            GetSeriesInstanceUIDs(directory);
+            GetFileNames(directory);
         }
     }
 }
